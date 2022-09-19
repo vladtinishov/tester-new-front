954254dc81dd945d8a2626c032394645b312e573
vladislavtinishov <vladislav04022001@gmail.com>
2022-09-20 01:25:33 +0600

    vue-demi resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,31 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
-import Unocss from 'unocss/vite'
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import importToCDN, { autoComplete } from 'vite-plugin-cdn-import2'
+import Unocss from "unocss/vite";
 import {
   presetAttributify,
   presetIcons,
   presetUno,
   transformerDirectives,
   transformerVariantGroup,
-} from 'unocss'
+} from "unocss";
 
-const pathSrc = path.resolve(__dirname, 'src')
+const pathSrc = path.resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: [
-        'vue-demi',
-      ]
-    }
+      external: ["vue-demi"],
+    },
   },
   resolve: {
     alias: {
-      '~/': `${pathSrc}/`,
+      "~/": `${pathSrc}/`,
     },
   },
   css: {
@@ -39,18 +37,33 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    importToCDN({
+      modules: [
+        autoComplete("vue"),
+        {
+          name: "vue-demi",
+          var: "VueDemi",
+          path: "lib/index.iife.min.js",
+        },
+        {
+          name: "pinia",
+          var: "Pinia",
+          path: "dist/pinia.iife.min.js",
+        },
+      ],
+    }),
     Components({
-      dirs: ['src/widgets', 'src/entities', 'src/features', 'src/widgets'],
+      dirs: ["src/widgets", "src/entities", "src/features", "src/widgets"],
       // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
+      extensions: ["vue", "md"],
       // allow auto import and register components used in markdown
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       resolvers: [
         ElementPlusResolver({
-          importStyle: 'sass',
+          importStyle: "sass",
         }),
       ],
-      dts: 'src/components.d.ts',
+      dts: "src/components.d.ts",
     }),
 
     // https://github.com/antfu/unocss
@@ -64,10 +77,7 @@ export default defineConfig({
           warn: true,
         }),
       ],
-      transformers: [
-        transformerDirectives(),
-        transformerVariantGroup(),
-      ]
+      transformers: [transformerDirectives(), transformerVariantGroup()],
     }),
   ],
-})
+});
